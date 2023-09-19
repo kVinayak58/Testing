5c440fbf36a1753b7b3ff2fc5255e467460fd818
kVinayak58 <131067253+kVinayak58@users.noreply.github.com>
2023-09-19 16:13:33 +0530

    First Commit

---
+++ PythonScript.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+x = int(input("Enter value for x:"))
+
+if x > 0:
+    print("X is: ",x)
+if x < 0:
+    print("X is not greater than 0, x is", x)
+
+
+if x == 0: 
+    print("X is 0")
